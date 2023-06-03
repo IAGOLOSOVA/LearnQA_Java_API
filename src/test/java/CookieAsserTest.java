ca59ba428b03f86e4a8b8517e4b8f988c44bebf1
IAGolosova <ira.mimoza@gmail.com>
2023-06-04 00:18:54 +0300

    Ex11 Asser Cookie "HomeWork"

---
+++ src/test/java/CookieAsserTest.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import java.util.Map;
+
+public class CookieAsserTest {
+    @Test
+    public void CookieGet (){
+
+        Response response = RestAssured
+                .get(" https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        Map<String,String> responseCookies = response.getCookies();
+        System.out.println(responseCookies);
+        
+        assertEquals(200, response.statusCode(), "Unexpected status code");
+        assertTrue(responseCookies.containsKey("HomeWork"),"Response doesn't have 'HomeWork' cookie");
+    }
+}
