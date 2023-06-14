f7209a67b09d296496d4ea26936b63ce9f882e9b
IAGolosova <ira.mimoza@gmail.com>
2023-06-14 13:18:33 +0300

    Ex16 New user

--- src/test/java/lib/DataGeneration.java
+++ src/test/java/lib/DataGeneration.java
@@ -1,6 +1,7 @@
 package lib;
 
 import java.text.SimpleDateFormat;
+import java.util.Random;
 
 public class DataGeneration {
     public static String getRandomEmail() {
@@ -8,4 +9,21 @@ public class DataGeneration {
         return "learnqa"+ timestamp + "@example.com";
 
     }
+
+    public static String getRandomName (String n) {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 0;
+        targetStringLength = Integer.parseInt(n);
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+            .limit(targetStringLength)
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
+        return generatedString;
+
+    }
 }
+
+ 
--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -1,103 +1,160 @@
 package tests;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import lib.BaseTestCase;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.DataGeneration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
-public class UserRegisterTest extends BaseTestCase{
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
+
+@Epic("Registration test")
+@Feature("Registration")
+
+public class UserRegisterTest extends BaseTestCase {
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
     @Test
-    public void testCresteUserWithExistingEmail() {
+    @Description("This test not successfully to registrate authorize user with existing email")
+    @DisplayName("Test negative create user")
+    public void testCreateUserWithExistingEmail() {
         String email = "vinkotov@example.com";
 
-        Map<String,String> userData = new HashMap<>();
+        Map<String, String> userData = new HashMap<>();
         userData.put("email", email);
         userData.put("password", "123");
         userData.put("username", "learnqa");
         userData.put("firstName", "learnqa");
         userData.put("lastName", "learnqa");
 
-        Response responseCreatAuth = RestAssured
-                .given()
-                .body(userData)
-                .post("https://playground.learnqa.ru/api/user/")
-                .andReturn();
-
-        Assertions.asserResponseCodeEquals(responseCreatAuth,400);
-        Assertions.asserResponseTextEquals(responseCreatAuth, "Users with email '"+ email+ "' already exists");
+        Response responseCreatAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+        Assertions.asserResponseCodeEquals(responseCreatAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreatAuth, "Users with email '" + email + "' already exists");
     }
 
     @Test
+    @Description("This test successfully to registrate user with new email")
+    @DisplayName("Test positive create user")
     public void testCresteUserSuccessfully() {
         String email = DataGeneration.getRandomEmail();
 
-        Map<String,String> userData = new HashMap<>();
+        Map<String, String> userData = new HashMap<>();
         userData.put("email", email);
         userData.put("password", "123");
         userData.put("username", "learnqa");
         userData.put("firstName", "learnqa");
         userData.put("lastName", "learnqa");
 
-        Response responseCreatAuth = RestAssured
-                .given()
-                .body(userData)
-                .post("https://playground.learnqa.ru/api/user/")
-                .andReturn();
-
-        Assertions.asserResponseCodeEquals(responseCreatAuth,200);
-        Assertions.assertJsonHasKey(responseCreatAuth, "id");
+        Response responseCreatAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+        Assertions.asserResponseCodeEquals(responseCreatAuth, 200);
+        Assertions.assertJsonHasField(responseCreatAuth, "id");
     }
 
     @Test
-    public void testCresteUserWithBadEmail() {
+    @Description("This test not successfully to registrate user with email without @")
+    @DisplayName("Test negative create user with invalid email")
+    public void testCresteUserWithInvalidEmail() {
         String email = "vinkotovexample.com";
 
-        Map<String,String> userData = new HashMap<>();
+        Map<String, String> userData = new HashMap<>();
         userData.put("email", email);
         userData.put("password", "123");
         userData.put("username", "learnqa");
         userData.put("firstName", "learnqa");
         userData.put("lastName", "learnqa");
 
-        Response responseCreatAuth = RestAssured
-                .given()
-                .body(userData)
-                .post("https://playground.learnqa.ru/api/user/")
-                .andReturn();
-//        System.out.println(responseCreatAuth.asString());
-//        System.out.println(responseCreatAuth.statusCode());   
-        Assertions.asserResponseCodeEquals(responseCreatAuth,400);
-        Assertions.asserResponseTextEquals(responseCreatAuth, "Invalid email format");  
-    }    
+        Response responseCreatAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+        Assertions.asserResponseCodeEquals(responseCreatAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreatAuth, "Invalid email format");
+    }
 
     @ParameterizedTest
-    @CsvSource ({'"email","})
-    public void testCresteUserWithBadEmail() {
-        String email = "vinkotovexample.com";
+    @Description("This test not successfully to registrate user without any parametr")
+    @DisplayName("Test negative create user with invalid email")
+    @ValueSource(strings = { "email", "password", "username", "firstName", "lastName" })
+    public void testCresteUserWithoutAnyParam(String param) {
+        String email = DataGeneration.getRandomEmail();
 
-        Map<String,String> userData = new HashMap<>();
-        userData.put("email", email);
+        Map<String, String> userData = new HashMap<>();
+        if (!param.equals("email")) {
+            userData.put("email", email);
+        }
+        if (!param.equals("password")) {
+            userData.put("password", "123");
+        }
+        if (!param.equals("username")) {
+            userData.put("username", "learnqa");
+        }
+        if (!param.equals("firstName")) {
+            userData.put("firstName", "learnqa");
+        }
+        if (!param.equals("lastName")) {
+            userData.put("lastName", "learnqa");
+        }
+
+        // userData.put(param,"");
+        // System.out.print(userData);
+        Response responseCreatAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+
+        // responseCreatAuth.prettyPrint();
+        Assertions.asserResponseCodeEquals(responseCreatAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreatAuth, "The following required params are missed: " + param);
+    }
+
+    @Test
+    @Description("This test not successfully to registrate user with short name")
+    @DisplayName("Test negative create user with short name")
+    public void testCresteUserWithShortName() {
+        //String name = " ";
+        String name = DataGeneration.getRandomName("1");
+        System.out.println(name);
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", "vinkotov@example.com");
         userData.put("password", "123");
-        userData.put("username", "learnqa");
+        userData.put("username", name);
         userData.put("firstName", "learnqa");
         userData.put("lastName", "learnqa");
 
-        Response responseCreatAuth = RestAssured
-                .given()
-                .body(userData)
-                .post("https://playground.learnqa.ru/api/user/")
-                .andReturn();
-//        System.out.println(responseCreatAuth.asString());
-//        System.out.println(responseCreatAuth.statusCode());   
-        Assertions.asserResponseCodeEquals(responseCreatAuth,400);
-        Assertions.asserResponseTextEquals(responseCreatAuth, "Invalid email format");  
-    }    
+        Response responseCreatAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+        //responseCreatAuth.prettyPrint();        
+        Assertions.asserResponseCodeEquals(responseCreatAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreatAuth, "The value of 'username' field is too short");
+    }
     
-    
+    @Test
+    @Description("This test not successfully to registrate user with name longer 250")
+    @DisplayName("Test negative create user with name longer 250")
+    public void testCresteUserWithLongName() {
+        
+        String name = DataGeneration.getRandomName("251");
+        System.out.println(name);
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", "vinkotov@example.com");
+        userData.put("password", "123");
+        userData.put("username", name);
+        userData.put("firstName", "learnqa");
+        userData.put("lastName", "learnqa");
+
+        Response responseCreatAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+        //responseCreatAuth.prettyPrint();        
+        Assertions.asserResponseCodeEquals(responseCreatAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreatAuth, "The value of 'username' field is too long");
+    }
 }
